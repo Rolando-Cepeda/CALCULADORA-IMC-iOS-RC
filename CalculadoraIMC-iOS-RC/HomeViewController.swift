4d4ff9fd06fd65447a3945f14c4e2d82d1e2833e
rolando_cf <rolandodj83@gmail.com>
2024-07-23 14:07:31 +0200

    Primera version

--- CalculadoraIMC-iOS-RC/HomeViewController.swift
+++ CalculadoraIMC-iOS-RC/HomeViewController.swift
@@ -7,15 +7,40 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeViewController:UIViewController {
+    
 
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    
+        //Aqui van las propiedades
+    //Para tener las referencias, cuando cambien los valores
+    var height: Int = 160
+    var weight: Float = 70
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     
     }
+    
+        //Aqui van los metodos(Action)
 
-        
+    @IBAction func onHeightChanged(_ sender: UIStepper) {
+        //Transformamos un double a entero
+        //En Android se utiliza .toInt()
+        height = Int(sender.value)
+        heightLabel.text = "\(sender.value) cm"
+    }
+    
 
+    @IBAction func onWeightChanged(_ sender: UISlider) {
+        weight = sender.value
+        weightLabel.text = "\(sender.value) kg"
+    }
+    
+    
+    @IBAction func calculate(_ sender: Any) {
+    }
 }
 
