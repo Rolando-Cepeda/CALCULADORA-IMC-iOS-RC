120ed0404a4709026a1ea3d3f4245c53d6439152
rolando_cf <rolandodj83@gmail.com>
2024-07-23 11:59:50 +0200

    Código modificado

---
+++ CalculadoraIMC-iOS-RC/HomeViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  CalculadoraIMC-iOS-RC
+//
+//  Created by Mañanas on 22/7/24.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    
+    }
+
+        
+
+}
+
